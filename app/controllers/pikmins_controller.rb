b7cfb217dad3e1fd5567aab4d1ca28e18c8af540
Margarida Almeida <margarida.almeida.92@gmail.com>
2023-09-25 17:55:56 +0100

    changes

---
+++ app/controllers/pikmins_controller.rb
@@ -0,0 +1,2 @@
+class PikminsController < ApplicationController
+end
---
+++ test/controllers/pikmins_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PikminsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
